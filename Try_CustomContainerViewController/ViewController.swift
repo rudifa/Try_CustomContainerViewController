335894e83bbcccceb304a81439cab33489b2208f
rudifa <rudi.farkas@gmail.com>
2018-06-22 21:31:39 +0200

    Initial Commit

---
+++ Try_CustomContainerViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Try_CustomContainerViewController
+//
+//  Created by Rudolf Farkas on 22.06.18.
+//  Copyright © 2018 Rudolf Farkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
